858cb00ca042be7ff473c54f948493d82edcbf15
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-08-23 16:39:34 +0900

    forward kinematics code

--- kinematics.py
+++ kinematics.py
@@ -8,16 +8,29 @@ def rand_theta():
 def rand_arm():
     return np.random.uniform(0, 10)
 
-thetas = [
+all_thetas = [
     rand_theta(),
     rand_theta(),
     rand_theta()
 ]
-arms = [
+all_arms = [
     rand_arm(),
     rand_arm(),
     rand_arm()
 ]
 
-thetas = np.array(thetas).reshape(1, -1)
-arms = np.array(arms).reshape(1, -1)
+def forward_kinematics(thetas, arms):
+    thetas = np.array(thetas).reshape(1, -1)
+    arms = np.array(arms)
+
+    thetas_acc = thetas @ np.tri(len(arms)).T
+    thetas_acc = thetas_acc.flatten()
+
+    x = np.cos(thetas_acc) @ arms
+    y = np.sin(thetas_acc) @ arms
+
+    return x, y
+
+print(forward_kinematics(all_thetas[:1], all_arms[:1]))
+print(forward_kinematics(all_thetas[:2], all_arms[:2]))
+print(forward_kinematics(all_thetas[:3], all_arms[:3]))
